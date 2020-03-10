9db89569103936059c7e087d45641fb8e2ef05db
Orange-cake <29853148+Orange-cake@users.noreply.github.com>
2020-03-10 14:43:50 -0400

    Initial commit

---
+++ src/model/Controller.java
@@ -0,0 +1,14 @@
+package model;
+
+import impresario.IView;
+
+public class Controller implements IView {
+    public Controller(){
+
+    }
+
+    @Override
+    public void updateState(String key, Object value) {
+
+    }
+}
---
+++ src/userinterface/ViewFactory.java
@@ -0,0 +1,27 @@
+package userinterface;
+
+import impresario.IModel;
+
+//==============================================================================
+public class ViewFactory {
+
+	public static View createView(String viewName, IModel model)
+	{
+		//TODO: Call constructor for class based on viewName
+		return null;
+	}
+
+
+	/*
+	public static Vector createVectorView(String viewName, IModel model)
+	{
+		if(viewName.equals("SOME VIEW NAME") == true)
+		{
+			//return [A NEW VECTOR VIEW OF THAT NAME TYPE]
+		}
+		else
+			return null;
+	}
+	*/
+
+}
