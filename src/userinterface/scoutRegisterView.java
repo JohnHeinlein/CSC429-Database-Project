4b1944186c9e33478ff0a2ed39d5af64a1ee6087
Orange-cake <29853148+Orange-cake@users.noreply.github.com>
2020-03-11 21:23:06 -0400

    Vulgar test view

--- src/userinterface/scoutRegisterView.java
+++ src/userinterface/scoutRegisterView.java
@@ -2,9 +2,14 @@ package userinterface;
 
 import impresario.IModel;
 
-public class scoutRegisterView extends View{
+public class scoutRegisterView extends GenericView{
     public scoutRegisterView(IModel model) {
         super(model, "scoutRegisterView");
+
+        setTitle("Register a scout");
+        addContent("Fuck you",
+                makeButt("Big penis", e -> System.out.println("OH LAWD")));
+        cancelButton();
     }
 
     @Override
