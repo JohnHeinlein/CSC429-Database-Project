eb660b8d05c1267c5f31f3720978425a3d001f7c
john <john.m.heinlein@gmail.com>
2020-03-17 11:41:55 -0400

    Move debugging method to its own class

---
+++ src/Debug.java
@@ -0,0 +1,26 @@
+/**
+ * Class containing static methods to aid in debug logs
+ * call setDebug(true) in the main method to enable logErr calls
+ */
+
+public class Debug {
+    public static boolean debug;
+
+    public static void setDebug(Boolean val){
+        debug = val;
+    }
+
+    /**
+     * Prints the class and method name this function is called from, plus a message
+     * @param msg Message to append to stack trace
+     */
+    public static void logErr(String msg){
+        if(!debug) return; //Return if we're not debugging
+
+        StackTraceElement[] ex = new Exception().getStackTrace();
+        System.err.printf("%s.%s: %s\n",
+                ex[1].getClassName(),   // 0 is this method and class
+                ex[1].getMethodName(),
+                msg);
+    }
+}
