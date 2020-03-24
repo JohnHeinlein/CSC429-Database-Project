f31fdd10777ed8940fb2f8d1a44061bec894ffe8
john <john.m.heinlein@gmail.com>
2020-03-24 15:55:14 -0400

    Stefen overwrote this, the silly man

--- src/utilities/Debug.java
+++ src/utilities/Debug.java
@@ -8,26 +8,39 @@ public class Debug {
 
     public static boolean debug;
 
-    public static Boolean setDebug(Boolean val){
+    public static Boolean setDebug(Boolean val) {
         debug = val;
         return debug;
     }
 
-    public static void logMsg(String messasge) {
-        System.out.println(messasge);
-    }
+    /**
+     * Prints the class and method name this function is called from, plus a message, as red text
+     *
+     * @param msg Message to append to stack trace
+     */
+    public static void logErr(String msg) { log(msg,true); }
 
     /**
      * Prints the class and method name this function is called from, plus a message
+     *
      * @param msg Message to append to stack trace
      */
-    public static void logErr(String msg){
-        if(!debug) return; //Return if we're not debugging
+    public static void logMsg(String msg) { log(msg,false); }
+
+    private static void log(String msg, Boolean isError){
+        if(!debug) return;
 
         StackTraceElement[] ex = new Exception().getStackTrace();
-        System.err.printf("%s.%s: %s\n",
-                ex[1].getClassName(),   // 0 is this method and class
-                ex[1].getMethodName(),
-                msg);
+        if(isError) {
+            System.err.printf("[%s.%s] %s\n", //Prints as red error text to console
+                    ex[2].getClassName(),   // 0 is this method and class
+                    ex[2].getMethodName(),  // 1 is the above methods
+                    msg);
+        }else{
+            System.out.printf("[%s.%s] %s\n", //Prints as normal text
+                    ex[2].getClassName(),
+                    ex[2].getMethodName(),
+                    msg);
+        }
     }
 }
